2d2224c30894cab1828770292bc1e5989725b7a3
happygreenfrog <41273098+happygreenfrog@users.noreply.github.com>
2025-08-31 17:05:24 -0500

    Removed an unused function

--- d1/main/menu.c
+++ d1/main/menu.c
@@ -820,7 +820,6 @@ int do_new_game_menu()
 		return 0;
 
 	//new stuff for loading game automatically if possible -happygreenfairy
-	//char apworldtestchar[15] = "apworldtest";
 	if(PlayerCfg.AutosaveAutoload == 1) {
 		if (!ap_state_restore_all(0, apworldname, new_level_num)) { StartNewGame(new_level_num); }
 	}
